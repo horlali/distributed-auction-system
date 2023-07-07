6e12da0d9e93937bb05bc4f61cb8a1bf7bd19086
horlali <gideon.ahiadzi@gmail.com>
2023-07-07 11:55:21 +0000

    replaced

--- auction_system/models/database.py
+++ auction_system/models/database.py
@@ -1,32 +1,58 @@
-from sqlmodel import Field, Session, SQLModel, create_engine
+from enum import Enum
+
+from sqlalchemy import Column, DateTime
+from sqlalchemy import Enum as db_enum
+from sqlalchemy import Float, ForeignKey, Integer, String, create_engine
+from sqlalchemy.orm import Session, declarative_base, relationship
+
+Base = declarative_base()
 
 
-# Define the SQLModel classes for the tables
-class User(SQLModel, table=True):
-    id: int = Field(default=None, primary_key=True)
-    username: str
-    password: str
-    role: str
+class UserType(Enum):
+    SELLER: str = "seller"
+    BIDDER: str = "bidder"
 
 
-class Auction(SQLModel, table=True):
-    id: int = Field(default=None, primary_key=True)
-    item_name: str
-    starting_price: float
-    reserve_price: float
-    start_time: str
-    end_time: str
-    seller_id: int
+class AuctionStatus(Enum):
+    ACTIVE = "active"
+    CLOSED = "closed"
 
 
-class Bid(SQLModel, table=True):
-    id: int = Field(default=None, primary_key=True)
-    auction_id: int
-    bidder_id: int
-    amount: float
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    email = Column(String(120), unique=True, nullable=False)
+    password_hash = Column(String(128), nullable=False)
+    user_type = Column(db_enum(UserType), nullable=False)
 
 
-# Create the database engine and session
-sqlite_url = "sqlite:///auction_system.db"
+class Auction(Base):
+    __tablename__ = "auctions"
+    id = Column(Integer, primary_key=True)
+    title = Column(String(100), nullable=False)
+    description = Column(String(500), nullable=False)
+    start_time = Column(DateTime, nullable=False)
+    end_time = Column(DateTime, nullable=False)
+    starting_price = Column(Float, nullable=False)
+    reserved_price = Column(Float, nullable=False)
+    seller = Column(Integer, ForeignKey("users.id"), nullable=False)
+    status = Column(db_enum(AuctionStatus), default=AuctionStatus.ACTIVE, nullable=False)
+
+    users = relationship("User", backref="auction", lazy=True)
+
+
+class Bid(Base):
+    __tablename__ = "bids"
+    id = Column(Integer, primary_key=True)
+    amount = Column(Float, nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    auction_id = Column(Integer, ForeignKey("auctions.id"), nullable=False)
+
+    users = relationship("User", backref="bid", lazy=True)
+
+
+sqlite_url = "sqlite:///db.sqlite3"
 engine = create_engine(sqlite_url)
 session = Session(engine)
+
+Base.metadata.create_all(engine)
