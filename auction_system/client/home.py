130cb980ac8ea47662269df49813cb12bc0efa3c
se7en <gideon.ahiadzi@gmail.com>
2023-07-13 09:40:50 +0000

    exception handling

--- auction_system/client/home.py
+++ auction_system/client/home.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from typing import Dict, List
 
 import streamlit as st
+from Pyro4.errors import CommunicationError
 
 from auction_system.client.components.connections import auction_bid_object, cart_image
 
@@ -12,50 +13,57 @@ st.write("# Welcome to the Special Auction System 👋")
 def show_auctions():
     st.write("#### Recent Active Auctions")
     st.divider()
-    auctions: List[Dict] = auction_bid_object.get_all_active_auctions()
 
-    for auction in auctions:
-        col1, col2, col3 = st.columns([1, 3, 1])
-        with col1:
-            st.image(cart_image, width=150)
+    try:
+        auctions: List[Dict] = auction_bid_object.get_all_active_auctions()
 
-        with col2:
-            start_time = datetime.fromisoformat(
-                auction["start_time"],
-            ).strftime("%B %d, %Y %I:%M %p")
+        for auction in auctions:
+            col1, col2, col3 = st.columns([1, 3, 1])
+            with col1:
+                st.image(cart_image, width=150)
 
-            end_time = datetime.fromisoformat(
-                auction["end_time"],
-            ).strftime("%B %d, %Y %I:%M %p")
+            with col2:
+                start_time = datetime.fromisoformat(
+                    auction["start_time"],
+                ).strftime("%B %d, %Y %I:%M %p")
 
-            st.text(f"Item title: {auction['title']}")
-            st.text(f"Item description: {auction['description']}")
-            st.text(f"Starting price: {auction['starting_price']}")
-            st.text(f"Auction status: {auction['status']}")
-            st.text(f"Auction started at: {start_time}")
-            st.text(f"Auction ends at: {end_time}")
-            st.text(f"Seller ID: {auction['seller_id']}")
+                end_time = datetime.fromisoformat(
+                    auction["end_time"],
+                ).strftime("%B %d, %Y %I:%M %p")
 
-        with col3:
-            # create a button to place a bid
-            if st.button("Place a bid", key=auction["id"]):
-                bid_amount = st.number_input("Enter your bid amount", min_value=0.0)
+                st.markdown(f"**Item title:** {auction['title']}")
+                st.text(f"Item description: {auction['description']}")
+                st.text(f"Starting price: {auction['starting_price']}")
+                st.text(f"Auction status: {auction['status']}")
+                st.text(f"Auction started at: {start_time}")
+                st.text(f"Auction ends at: {end_time}")
+                st.text(f"Seller ID: {auction['seller_id']}")
 
-                # create a bid object
-                if bid_amount:
-                    bid_data = {
-                        "amount": bid_amount,
-                        "bidder_id": auction["seller_id"],
-                        "auction_id": auction["id"],
-                    }
+            with col3:
+                # create a button to place a bid
+                if st.button("Place a bid", key=auction["id"]):
+                    bid_amount = st.number_input("Enter your bid amount", min_value=0.0)
 
-                    # place the bid
-                    bid = auction_bid_object.place_bid(bid_data)
-                    st.write(bid)
-                    st.success(f"Your bid has been placed successfully: {bid}")
+                    # create a bid object
+                    if bid_amount:
+                        bid_data = {
+                            "amount": bid_amount,
+                            "bidder_id": auction["seller_id"],
+                            "auction_id": auction["id"],
+                        }
 
-                    # refresh the page
-                    st.experimental_rerun()
+                        # place the bid
+                        bid = auction_bid_object.place_bid(bid_data)
+                        st.write(bid)
+                        st.success(f"Your bid has been placed successfully: {bid}")
+
+                        # refresh the page
+                        st.experimental_rerun()
+
+            st.divider()
+
+    except CommunicationError:
+        st.error("Server is not running. Please start the server first.")
         st.divider()
 
 
