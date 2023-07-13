b9bcd96e6d9911dbfdc1072f5345699ac1d88575
se7en <gideon.ahiadzi@gmail.com>
2023-07-13 09:59:00 +0000

    changed text to write

--- auction_system/client/home.py
+++ auction_system/client/home.py
@@ -16,7 +16,6 @@ def show_auctions():
 
     try:
         auctions: List[Dict] = auction_bid_object.get_all_active_auctions()
-
         for auction in auctions:
             col1, col2, col3 = st.columns([1, 3, 1])
             with col1:
@@ -31,19 +30,18 @@ def show_auctions():
                     auction["end_time"],
                 ).strftime("%B %d, %Y %I:%M %p")
 
-                st.markdown(f"**Item title:** {auction['title']}")
-                st.text(f"Item description: {auction['description']}")
-                st.text(f"Starting price: {auction['starting_price']}")
-                st.text(f"Auction status: {auction['status']}")
-                st.text(f"Auction started at: {start_time}")
-                st.text(f"Auction ends at: {end_time}")
-                st.text(f"Seller ID: {auction['seller_id']}")
+                st.write(f"**Item title:** {auction['title']}")
+                st.write(f"**Item description:** {auction['description']}")
+                st.write(f"**Starting price:** {auction['starting_price']}")
+                st.write(f"**Auction status:** {auction['status']}")
+                st.write(f"**Auction started at:** {start_time}")
+                st.write(f"**Auction ends at:** {end_time}")
+                st.write(f"**Seller ID:** {auction['seller_id']}")
 
             with col3:
                 # create a button to place a bid
                 if st.button("Place a bid", key=auction["id"]):
                     bid_amount = st.number_input("Enter your bid amount", min_value=0.0)
-
                     # create a bid object
                     if bid_amount:
                         bid_data = {
