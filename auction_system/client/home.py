fba8fed1f2904fab2848ad533ea087eddfd7d3aa
se7en <gideon.ahiadzi@gmail.com>
2023-07-15 16:19:00 +0000

    fixed the damn checkout bug

--- auction_system/client/home.py
+++ auction_system/client/home.py
@@ -5,6 +5,7 @@ import streamlit as st
 from Pyro4.errors import CommunicationError
 
 from auction_system.client.components.connections import auction_bid_object, cart_image
+from auction_system.utils.extentions import element_keys
 
 st.set_page_config(page_title="Auction System", page_icon="👋", layout="wide")
 st.write("# Welcome to the Special Auction System 👋")
@@ -40,24 +41,20 @@ def show_auctions():
                 st.write(f"**Seller ID:** {auction['seller_id']}")
 
             with col3:
-                # create a button to place a bid
-                if st.button("Place a bid", key=auction["id"]):
-                    bid_amount = st.number_input("Enter your bid amount", min_value=0.0)
-                    # create a bid object
-                    if bid_amount:
-                        bid_data = {
-                            "amount": bid_amount,
-                            "bidder_id": auction["seller_id"],
-                            "auction_id": auction["id"],
-                        }
+                bid_amount = st.number_input(
+                    "Enter your bid amount", min_value=0.0, key=element_keys()
+                )
 
-                        # place the bid
-                        bid = auction_bid_object.place_bid(bid_data)
-                        st.write(bid)
-                        st.success(f"Your bid has been placed successfully: {bid}")
+                if st.button("Checkout", key=element_keys()):
+                    bid_data = {
+                        "amount": bid_amount,
+                        "bidder_id": auction["seller_id"],
+                        "auction_id": auction["id"],
+                    }
 
-                        # refresh the page
-                        st.experimental_rerun()
+                    # place the bid
+                    bid = auction_bid_object.place_bid(bid_data)
+                    st.success(f"Your bid has been placed successfully: {bid}")
 
             st.divider()
 
---
+++ auction_system/utils/extentions.py
@@ -0,0 +1,5 @@
+from uuid import uuid4
+
+
+def element_keys():
+    return str(uuid4().hex)
