ea413ffae92bfa0eafef0f269e756503bd77d46c
se7en <gideon.ahiadzi@gmail.com>
2023-07-21 14:58:03 +0000

    added signup functionality

--- auction_system/client/components/states.py
+++ auction_system/client/components/states.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from Pyro4.errors import CommunicationError
 
 from auction_system.client.components.connections import auth_object
 
@@ -35,8 +36,48 @@ def button_login():
             except TypeError:
                 st.error("Incorrect email or password")
 
+            except CommunicationError:
+                st.error("Could not connect to server")
+
     else:
         st.write("You are already logged in.")
+
+
+def button_signup():
+    if not st.session_state["authenticated"]:
+        form = st.form(key="signup_form")
+        first_name = form.text_input("First Name")
+        last_name = form.text_input("Last Name")
+        email = form.text_input("Email")
+        phone = form.text_input("Phone")
+        password = form.text_input("Password", type="password")
+        user_type = form.selectbox("User Type", ["seller", "buyer"])
+
+        if form.form_submit_button(label="Sign Up"):
+            try:
+                user_data = {
+                    "first_name": first_name,
+                    "last_name": last_name,
+                    "email": email,
+                    "phone": phone,
+                    "password": password,
+                    "user_type": user_type,
+                }
+                user_id, user_option, fname = auth_object.register(**user_data)
+                st.session_state["user_id"] = user_id
+                st.session_state["first_name"] = fname
+                st.session_state["user_type"] = user_option
+                st.session_state["authenticated"] = True
+                st.experimental_rerun()
+
+            except TypeError:
+                st.error("Error signing up. Please try again.")
+
+            except CommunicationError:
+                st.error("Could not connect to server")
+
+    else:
+        st.write("You are already logged in / sign up.")
 
 
 def button_logout():
@@ -55,4 +96,11 @@ def authentication_state():
         button_logout()
 
     else:
-        button_login()
+        col1, col2 = st.columns(2)
+        with col1:
+            st.header("Login")
+            button_signup()
+
+        with col2:
+            st.header("Sign Up")
+            button_login()
